2a924301c0085983b8728791a63b89f9ded85870
manuelolea <olea.fregoso@gmail.com>
2025-11-28 14:42:17 -0500

    classe fenettre, maison, porte, barreJeu rt particulescharge

--- src/main/java/sim/bdeb/qc/ca/demo/barreJeu.java
+++ src/main/java/sim/bdeb/qc/ca/demo/barreJeu.java
@@ -1,5 +1,6 @@
 package sim.bdeb.qc.ca.demo;
 
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 
 public class barreJeu {
@@ -9,12 +10,28 @@ public class barreJeu {
     protected int largeurBarre;
     protected Color couleurBarre;
 
-    public barreJeu( int largeurBarre, int hauteurBarre) {
+    private int journeauxRestants;
+    private int argent;
+    private Image imageArgent;
+    private Image imageMaison;
+    private Image imageJournal;
+
+    public barreJeu() {
         this.barrePosX = 0;
         this.barrePosY = 0;
         this.largeurBarre = 1300;
         this.couleurBarre = Color.rgb(0, 0, 0, 0.4);
         this.hauteurBarre = 30;
+        this.journeauxRestants= 24;
+        this.argent = 0;
+        this.imageArgent = new Image("icone-dollar.png");
+        this.imageJournal = new Image("icone-journal.png");
+        this.imageMaison = new Image("icone-maison.png");
 
     }
+
+    public void drawBarre(){
+
+    }
+
 }
