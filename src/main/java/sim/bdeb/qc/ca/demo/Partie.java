80d0fb8f78bc3e09c59a8da27ff055a3c852e0df
anouarfitas <anouar.fitas2007@gmail.com>
2025-12-01 14:43:29 -0500

    klas kldma
    Niveau dans partie

--- src/main/java/sim/bdeb/qc/ca/demo/Partie.java
+++ src/main/java/sim/bdeb/qc/ca/demo/Partie.java
@@ -8,6 +8,7 @@ import javafx.scene.text.Font;
 import javafx.scene.text.TextAlignment;
 
 import java.util.ArrayList;
+
 public class Partie {
     private Camelot camelot;
     private Decors decors;
@@ -17,8 +18,8 @@ public class Partie {
     private double hauteurEcran;
     private double largueurEcran;
 
-    private Niveau niveauActuel;
     private int numeroNiveau = 1;
+    private double finNiveauX;
 
     private boolean enChargement = true;
     private double tempsChargement = 3;
@@ -30,30 +31,30 @@ public class Partie {
     private double masse;
     private double tempsRecharge;
 
-    //private ArrayList<Maison> listeMaison;
+    private ArrayList<Maison> listeMaison;
+    private ArrayList<ParticulesCharges> listeParticule;
 
     private int argent = 0;
     private int nbJournaux = 0;
-    //private String chaineAdresse = "";
+    private String chaineAdresse = "";
 
-    public Partie(double largueur, double hauteur){
+    public Partie(double largueur, double hauteur) {
         this.largueurEcran = largueur;
         this.hauteurEcran = hauteur;
 
-        this.camera = new CameraJeu(0,0,largueur,hauteur);
+        this.camera = new CameraJeu(0, 0, largueur, hauteur);
         this.decors = new Decors();
-        this.camelot = new Camelot(180,436);
+        this.camelot = new Camelot(180, 436);
         this.barreJeu = new BarreJeu(largueur);
 
-        this.masse = 1 + Math.random();
         this.listeJournaux = new ArrayList<>();
-
-        //this.listeMaison = new ArrayList<>();
+        this.listeMaison = new ArrayList<>();
+        this.listeParticule = new ArrayList<>();
 
         initialiserNiveau(1);
-
     }
-    public void initialiserNiveau(int num){
+
+    public void initialiserNiveau(int num) {
         this.numeroNiveau = num;
         this.enChargement = true;
         this.tempsChargement = 3;
@@ -62,14 +63,58 @@ public class Partie {
         this.tempsFinPartie = 3;
 
         this.camelot.setPosition(180, 436);
-        this.camelot.setVitesse(0,0);
+        this.camelot.setVitesse(0, 0);
         this.camera.setX(0);
 
         this.nbJournaux += 12;
-
         this.masse = 1 + Math.random();
 
-        this.niveauActuel = new Niveau(num, largueurEcran, hauteurEcran);
+        listeMaison.clear();
+        listeParticule.clear();
+
+        genererMaison();
+        genererParticules();
+    }
+
+    private void genererMaison() {
+        int adresse = 100 + (int) (Math.random() * 850);
+        StringBuilder listetemporaire = new StringBuilder();
+        double derniereMaisonX = 0;
+
+        for (int i = 0; i < 12; i++) {
+            double maisonX = (i + 1) * 1300;
+            boolean estAbonne = Math.random() < 0.5;
+
+            Maison m = new Maison(maisonX, adresse, estAbonne);
+            listeMaison.add(m);
+
+            if (estAbonne) {
+                listetemporaire.append(adresse).append("  ");
+            }
+            adresse += 2;
+            derniereMaisonX = maisonX;
+        }
+        this.chaineAdresse = listetemporaire.toString();
+        this.finNiveauX = derniereMaisonX + (1.5 * largueurEcran);
+    }
+
+    private void genererParticules() {
+        int nombreParticules = 0;
+
+        if (numeroNiveau >= 2) {
+            int calcul = (numeroNiveau - 1) * 30;
+            nombreParticules = Math.min(calcul, 400);
+        }
+
+        if (nombreParticules <= 0) return;
+
+        double largueurTotalNiv = this.finNiveauX;
+
+        for (int i = 0; i < nombreParticules; i++) {
+            double rndX = Math.random() * largueurTotalNiv;
+            double rndY = Math.random() * hauteurEcran;
+            listeParticule.add(new ParticulesCharges(rndX, rndY));
+        }
     }
 
     public void update(double dt) {
@@ -96,6 +141,7 @@ public class Partie {
             finDePartie = true;
             return;
         }
+
         camelot.update(dt);
         decors.update(dt);
         camera.update(camelot);
@@ -105,32 +151,34 @@ public class Partie {
         for (int i = listeJournaux.size() - 1; i >= 0; i--) {
             Journaux j = listeJournaux.get(i);
             if (numeroNiveau >= 2) {
-                Point2D centreJournal = new Point2D(j.getX() + j.getLargueurJournal() / 2,
-                        j.getY() + j.getHauteurJournal() / 2);
+                Point2D centreJournal = new Point2D(
+                        j.getX() + j.getLargueurJournal() / 2,
+                        j.getY() + j.getHauteurJournal() / 2
+                );
 
                 Point2D forceElec = calculerChampsElectrique(centreJournal);
-                Point2D acceleration = forceElec.multiply(1 / j.getMasse());
+                Point2D acceleration = forceElec.multiply(1.0 / j.getMasse());
                 j.ajouterAcceleration(acceleration);
             }
             j.update(dt);
-
 
             if (gererCollision(j) || j.estSorti(camera)) {
                 listeJournaux.remove(i);
             }
         }
-        if (camelot.getX() > niveauActuel.getFinNiveauX()) {
+
+        if (camelot.getX() > this.finNiveauX) {
             initialiserNiveau(numeroNiveau + 1);
         }
     }
 
-    private Point2D calculerChampsElectrique(Point2D posJournal){
+    private Point2D calculerChampsElectrique(Point2D posJournal) {
         double k = 90;
         double qJournal = 900;
         double forceTotalX = 0;
         double forceTotalY = 0;
 
-        for (ParticulesCharges p : niveauActuel.getListeParticules()) {
+        for (ParticulesCharges p : listeParticule) {
             Point2D posParticule = p.getPosition();
             double qParticule = p.getCharge();
 
@@ -156,8 +204,9 @@ public class Partie {
         }
         return new Point2D(forceTotalX, forceTotalY);
     }
-    public boolean gererCollision(Journaux j){
-        for (Maison m : niveauActuel.getListeMaison()) {
+
+    public boolean gererCollision(Journaux j) {
+        for (Maison m : listeMaison) {
             boolean estAbonne = m.estAbonne();
 
             BoitesAuxLettres b = m.getBoitesAuxLettres();
@@ -180,11 +229,14 @@ public class Partie {
         return false;
     }
 
-    public boolean intercepte(Journaux j, double x, double y, double largueur, double hauteur){
-        return j.getX() < x + largueur && j.getX() + j.getLargueurJournal() > x && j.getY() < y + hauteur && j.getY() + j.getHauteurJournal() > y;
+    public boolean intercepte(Journaux j, double x, double y, double largueur, double hauteur) {
+        return j.getX() < x + largueur &&
+                j.getX() + j.getLargueurJournal() > x &&
+                j.getY() < y + hauteur &&
+                j.getY() + j.getHauteurJournal() > y;
     }
 
-    public void draw(GraphicsContext context){
+    public void draw(GraphicsContext context) {
         context.setFill(Color.BLACK);
         context.fillRect(0, 0, largueurEcran, hauteurEcran);
 
@@ -210,55 +262,34 @@ public class Partie {
             return;
         }
 
-        decors.draw(context,camera);
+        decors.draw(context, camera);
 
-        for (Maison m : niveauActuel.getListeMaison()) {
-            m.draw(context,camera);
+        for (Maison m : listeMaison) {
+            m.draw(context, camera);
+        }
+
+        for (ParticulesCharges p : listeParticule) {
+            p.draw(context, camera);
         }
 
         for (Journaux j : listeJournaux) {
             j.draw(context, camera);
         }
 
-        for (ParticulesCharges p : niveauActuel.getListeParticules()){
-            p.draw(context,camera);
-        }
-
-        camelot.draw(context,camera);
-        barreJeu.draw(context, nbJournaux, argent, niveauActuel.getChaineAdresse());
+        camelot.draw(context, camera);
+        barreJeu.draw(context, nbJournaux, argent, chaineAdresse);
     }
-    private void gererLancerJournaux(double dt){
-        if (tempsRecharge > 0){
+
+    private void gererLancerJournaux(double dt) {
+        if (tempsRecharge > 0) {
             tempsRecharge -= dt;
         }
-        if ((Input.lancerHaut || Input.lancerDroit) && tempsRecharge <= 0 && nbJournaux > 0){
+        if ((Input.lancerHaut || Input.lancerDroit) && tempsRecharge <= 0 && nbJournaux > 0) {
             Journaux journal = new Journaux(camelot, masse, Input.lancerHaut, Input.lancerDroit, Input.force);
             listeJournaux.add(journal);
 
             nbJournaux--;
             tempsRecharge = 0.5;
         }
-
     }
 }
-    /*private void genererNiveau(){
-        int adresse = 100 + (int)(Math.random() * 850);
-        StringBuilder listetemporaire = new StringBuilder();
-
-        for (int i = 0; i < 12; i++) {
-            double maisonX = (i + 1) * 1300;
-            boolean estAbonne = Math.random() < 0.5;
-
-            Maison m = new Maison(maisonX, adresse, estAbonne);
-            listeMaison.add(m);
-
-            if (estAbonne){
-                listetemporaire.append(adresse).append("  ");
-            }
-
-            adresse += 2;
-        }
-        this.chaineAdresse = listetemporaire.toString();
-    }
-}
-*/
