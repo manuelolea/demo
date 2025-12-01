dd4065e876a6e8b9eae039051f32b5253b6fe8f2
manuelolea <olea.fregoso@gmail.com>
2025-11-30 20:00:20 -0500

    Refactor BarreJeu constructor and clean up code
    Renamed constructor from 'barreJeu' to 'BarreJeu' and removed unused 'listeMaison' variable.

--- src/main/java/sim/bdeb/qc/ca/demo/barreJeu.java
+++ src/main/java/sim/bdeb/qc/ca/demo/barreJeu.java
@@ -11,13 +11,11 @@ public class barreJeu {
     private Image imageJournal;
     private Image imageArgent;
     private Image imageMaison;
-
-    // a ajouter dans la classse partie pour afficher dans la barre de jeu de type ( 208 400 573 679 399)
-    private String listeMaison;
-
-    public barreJeu(double largueurEcran) {
+    
+    public BarreJeu(double largueurEcran) {
         this.largeurEcran = largueurEcran;
         this.hauteurBarre = 40;
+
         this.imageArgent = new Image("icone-dollar.png");
         this.imageJournal = new Image("icone-journal.png");
         this.imageMaison = new Image("icone-maison.png");
